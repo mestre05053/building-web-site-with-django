3c76486ae767ca719ca8a25aab659425c2c394be
mestre05053 <63665431+mestre05053@users.noreply.github.com>
2020-04-17 10:57:25 -0500

    passing-variables-to-templates-OK

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-     print(request.headers)
-     return render (request, "personal/home.html" ) 
+
+     context = {}
+     list_of_values=[]
+     list_of_values.append("first entry")
+     list_of_values.append("second entry")
+     list_of_values.append("third entry")
+     list_of_values.append("fourth entry")
+     context['list_of_values'] = list_of_values
+
+     #context['some_string']="This is some string that Im passing to the view"
+     #context['some_number']=860623
+
+    
+
+
+
+     return render (request, "personal/home.html", context )
