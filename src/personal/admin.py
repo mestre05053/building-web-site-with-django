535d93e5af7d40526fa8d10df5a267882f67a1e5
mestre05053 <63665431+mestre05053@users.noreply.github.com>
2020-04-23 18:37:05 -0500

    todo salio bien tuve mis tropiezos con la identacion ya le cogi la vuelta., tengo que tener mas cuidado a la hora de escribir

--- src/personal/admin.py
+++ src/personal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from personal.models import Question
+from personal.models import Bikes 
+
+admin.site.register(Question)
+admin.site.register(Bikes)
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
-
+from personal.models import Question
+from personal.models import Bikes
 # Create your views here.
 
 def home_screen_view(request):
 
      context = {}
-     list_of_values=[]
-     list_of_values.append("first entry")
-     list_of_values.append("second entry")
-     list_of_values.append("third entry")
-     list_of_values.append("fourth entry")
-     context['list_of_values'] = list_of_values
+     # list_of_values=[]
+     # list_of_values.append("first entry")
+     # list_of_values.append("second entry")
+     # list_of_values.append("third entry")
+     # list_of_values.append("fourth entry")
+     # context['list_of_values'] = list_of_values
 
      #context['some_string']="This is some string that Im passing to the view"
      #context['some_number']=860623
 
     
+     questions = Question.objects.all()
+     context ['questions'] = questions
 
+     bikes = Bikes.objects.all()
+     context ['bikes'] = bikes
 
 
      return render (request, "personal/home.html", context )
